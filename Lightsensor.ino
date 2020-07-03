708c334c36b140015a5a2d2e39bd0a771a47c920
Mariana Pinto <47362393+Miknow78@users.noreply.github.com>
2020-07-03 12:10:10 +0100

    Add files via upload

--- Lightsensor.ino
+++ Lightsensor.ino
@@ -1,13 +1,26 @@
+#include <SPI.h>
+#include <Ethernet.h>
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; // endereço MAC disponivel na rede
+IPAdress ip(192, 168, 1, 69); // IP disponivel
+EthernetServer server(80) // porta 80
+
 #define lightSensor A0
 #define portao 7
 
+int curVal = 0;
 int ledFlag = 0;
 int lightlevel;
+
+String RString;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(portao, OUTPUT);
   digitalWrite(portao, HIGH);
+  Ethernet.begin(mac, ip);
+  server.begin();
 }
 
 /* delay corrigido
@@ -28,7 +41,8 @@ void LightDoIt(){
 }
 */
 void abrir(){
-  ledFlag = 3;
+  curVal = curVal + 1;
+  ledFlag = 3000;
   digitalWrite(portao, LOW);
 }
 
@@ -62,5 +76,56 @@ void loop(){
   else{
     fechar();
   }
-  delay(1000);
+
+  // SERVER com o modulo de Shield Ethernet W5100
+  EthernetClient client = server.available();
+  if(client){
+    boolean cLIB = true;
+    while (client.connected()){
+      if(client.available()){
+        char r = client.read();
+
+        if(RString.length() < 100) {
+          RString += r;
+        }
+        if (r == '\n' && cLIB){ // criar pagina HTML
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");
+          client.println();
+          client.println("<!DOCTYPE html>");
+          client.println("<html>");
+          client.println("<head>");
+          client.println("<title>Parque de estacionamento</title>");
+          client.println("</head>");
+          client.println("<body>");
+          client.println("<h1>Numero de ocupantes</h1>");
+          client.print("<p>");
+          client.print(curVal, DEC);
+          client.println("</p>");
+          client.println("<a href=/?on2 >ON</a>");
+          client.println("<a href=/?off3 >OFF</a>");
+          client.println("</body>");
+          client.println("</html>");
+          break;
+        }
+        if (r == '\n'){
+          cLIB = true;
+        }
+        else if (r != '\r'){
+          cLIB = false;
+        }
+      }
+    }
+  }
+  delay(1);
+  client.stop();
+  // href=...
+  if(RString.indexOf('3') >0){
+    abrir();
+  }
+  if(RString.indexOf('4') >0){
+    fechar();
+  }
+  RString="";
 }
